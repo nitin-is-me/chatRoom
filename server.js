e5fe398052578c6708f39bd195161c0b44b4a7a1
Nitin <asdfasdf>
2025-02-23 12:03:17 +0530

    Added a dozens of features and UI improvement

--- server.js
+++ server.js
@@ -2,18 +2,44 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const path = require('path');
-
+const axios = require("axios");
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+async function getIp(){
+    const response = await axios.get("https://api.ipify.org/?format=json");
+    return response.data.ip;
+}
+
+// Middleware to log requests, alternative to morgan
+// app.use((req, res, next) => {
+//     console.log(`${req.method} ${req.url}`);
+//     next();
+// });
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Store connected users
+const connectedUsers = new Set();
+
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    // console.log('a user connected');
 
     // Notify all users when someone joins
-    socket.on('join', (name) => {
+    socket.on('join', async (name) => {
+        if (connectedUsers.has(name)) {
+            // Notify the client that the name is already taken
+            socket.emit('name taken', name);
+            return;
+        }
+
+        // finding ip address of user
+        const ip = await getIp();
+        console.log(`${name} has joined. IP address: ${ip}`);
+
+        // Add the user to the list of connected users
+        connectedUsers.add(name);
         socket.username = name; // Store the username in the socket object
         io.emit('user joined', `${name} joined the room`);
     });
@@ -24,9 +50,27 @@ io.on('connection', (socket) => {
     });
 
     // Notify all users when someone leaves
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-        io.emit('user left', `${socket.username || 'A user'} left the room`);
+    socket.on('disconnect', async () => {
+        // console.log('a user disconnected');
+        const ip = await getIp();
+        console.log(`${socket.username} has left. IP address: ${ip}`);
+        if (socket.username) {
+            connectedUsers.delete(socket.username); // Remove the user from the list
+            io.emit('user left', `${socket.username} left the room`);
+        }
+    });
+});
+
+process.on('SIGINT', () => {
+    console.log('Server is shutting down...');
+
+    // Notify all clients that the server is shutting down
+    io.emit('server shutdown', 'Admin has shut down the server');
+
+    // Close the server
+    server.close(() => {
+        console.log('Server has been shut down.');
+        process.exit(0); // Exit the process (0 means without errors)
     });
 });
 
